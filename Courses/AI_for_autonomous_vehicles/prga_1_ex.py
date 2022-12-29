906d8b3476acb591f1b39b7d51c27825d37b0086
Srihari293 <76211104+Srihari293@users.noreply.github.com>
2022-12-29 16:39:30 +1100

    localization

---
+++ Courses/AI_for_autonomous_vehicles/prga_1_ex.py
@@ -0,0 +1,11 @@
+# Basic template
+colors = [['green','green','green'],
+          ['green','red','red'],
+          ['green','green','green']]
+
+measurements = ['red']
+motions = [[0,0],[0,1]]
+sensor_right = 0.8
+p_move = 1.0
+
+# Read colors, uniform dist, motion, measurement, final distribution
