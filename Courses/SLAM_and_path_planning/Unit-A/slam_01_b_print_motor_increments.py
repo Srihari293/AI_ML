aa561d965e5ebdf08572b41ad21f23aa0a9a332a
Srihari293 <ssrihari2002@gmail.com>
2023-01-04 23:02:13 +0530

    ticks difference left and right wheel

---
+++ Courses/SLAM_and_path_planning/Unit-A/slam_01_b_print_motor_increments.py
@@ -0,0 +1,10 @@
+# Print the increments of the left and right motor.
+# Now using the LegoLogfile class.
+# 01_b_print_motor_increments.py
+# Srihari Sridhar, 04-01-22
+from lego_robot import LegoLogfile  
+if __name__ == '__main__':
+    logfile = LegoLogfile()
+    logfile.read(r"C:\Users\sriha\OneDrive\Documents\Code\AI_ML\Courses\SLAM_and_path_planning\Unit-A\robot4_motors.txt")
+    for i in range(20):
+        print(logfile.motor_ticks[i])
