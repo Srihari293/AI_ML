a1adcd4ee15e9bbf6ba315175de4a3d22405a4f5
Srihari293 <ssrihari2002@gmail.com>
2023-01-05 16:42:17 +0530

    Update slam_01_b_print_motor_increments.py

--- Courses/SLAM_and_path_planning/Unit-A/slam_01_b_print_motor_increments.py
+++ Courses/SLAM_and_path_planning/Unit-A/slam_01_b_print_motor_increments.py
@@ -1,7 +1,7 @@
 # Print the increments of the left and right motor.
 # Now using the LegoLogfile class.
 # 01_b_print_motor_increments.py
-from lego_robot import LegoLogfile  
+from lego_robot import LegoLogfile
 if __name__ == '__main__':
     logfile = LegoLogfile()
     logfile.read(r"C:\Users\sriha\OneDrive\Documents\Code\AI_ML\Courses\SLAM_and_path_planning\Unit-A\robot4_motors.txt")
