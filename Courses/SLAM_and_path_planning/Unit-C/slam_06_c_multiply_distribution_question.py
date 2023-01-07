ef32ce68676780d6f8ac1d913c4283395fbaf3e8
Srihari293 <ssrihari2002@gmail.com>
2023-01-07 17:27:17 +0530

    Posterior distribution computation completed

--- Courses/SLAM_and_path_planning/Unit-C/slam_06_c_multiply_distribution_question.py
+++ Courses/SLAM_and_path_planning/Unit-C/slam_06_c_multiply_distribution_question.py
@@ -6,21 +6,27 @@ from distribution import *
 
 def multiply(a, b):
     """Multiply two distributions and return the resulting distribution."""
-
     # --->>> Put your code here.
-    
-    return a  # Modify this to return your result.
+    # normalization term
 
+    vals = []
+    for i in range(min(a.start(), b.start()), max(a.stop(), b.stop()) + 1):
+        vals.append((a.value(i) * b.value(i)))
+
+    # Modify this to return your result.
+    distribution = Distribution(min(a.offset, b.offset), vals)
+    distribution.normalize()
+    return distribution
 
 if __name__ == '__main__':
-    arena = (0,1000)
+    arena = (0, 1000)
 
     # Here is our assumed position. Plotted in blue.
     position_value = 400
     position_error = 100
     position = Distribution.triangle(position_value, position_error)
     plot(position.plotlists(*arena)[0], position.plotlists(*arena)[1],
-         color='b', linestyle='steps')
+         color='b', drawstyle='steps')
 
     # Here is our measurement. Plotted in green.
     # That is what we read from the instrument.
@@ -28,12 +34,12 @@ if __name__ == '__main__':
     measurement_error = 200
     measurement = Distribution.triangle(measured_value, measurement_error)
     plot(measurement.plotlists(*arena)[0], measurement.plotlists(*arena)[1],
-         color='g', linestyle='steps')
+         color='g', drawstyle='steps')
 
     # Now, we integrate our sensor measurement. Result is plotted in red.
-    position_after_measurement = multiply(position, measurement)
+    position_after_measurement = multiply(measurement, position)
     plot(position_after_measurement.plotlists(*arena)[0],
          position_after_measurement.plotlists(*arena)[1],
-         color='r', linestyle='steps')
+         color='r', drawstyle='steps')
 
     show()
